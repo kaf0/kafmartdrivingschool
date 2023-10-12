ab1649aad8e0d632cbd2cda06d32e075951aef11
Gertrude Seworh <111208672+kaf0@users.noreply.github.com>
2023-10-12 03:30:57 -0500

    scss

---
+++ assets/pages/datatables.init.js
@@ -0,0 +1,18 @@
+/*
+ Template Name: Kafmart - Best Driving School In The City
+ Author: Gertrude Seworh
+ File: Datatable js
+ */
+
+$(document).ready(function() {
+    $('#datatable').DataTable();
+
+    //Buttons examples
+    var table = $('#datatable-buttons').DataTable({
+        lengthChange: false,
+        buttons: ['copy', 'excel', 'pdf', 'colvis']
+    });
+
+    table.buttons().container()
+        .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
+} );
